dc98921bfc857048a64b274a6c892e8c915ad201
orych <nix>
2022-12-03 18:21:30 +0100

    last func

---
+++ posts.js
@@ -0,0 +1,58 @@
+let posts = [{
+        'author': 'Chantal Lumen',
+        'description': '',
+        'profileImg': 'src/img/profile/1.jpg',
+        'image': 'src/img/profile/b1.jpg',
+        'imgDesc': 'some stones yk',
+        'likes': '999',
+        'commAuth': ['Jacky', 'Shia LaBeouf'],
+        'comments': [
+            'Helllooo!',
+            'JUST- DO- IT'
+        ],
+    },
+
+    {
+        'author': 'Klaus Peter',
+        'description': 'Madeira, Portugal',
+        'profileImg': 'src/img/profile/4.jpg',
+        'image': 'src/img/profile/b3.jpg',
+        'imgDesc': 'Sonne, Strand, Portugal☀️🏖️',
+        'likes': '1.999',
+        'commAuth': ['Konstantin', 'Jens Josef'],
+        'comments': [
+            'Wunderbar!!',
+            '😍😍😍'
+        ],
+    },
+    {
+        'author': 'Hans Georg',
+        'description': 'Down Under',
+        'profileImg': 'src/img/profile/2.jpg',
+        'image': 'src/img/profile/b2.jpg',
+        'imgDesc': 'Look at this view! ',
+        'likes': '1.342.422',
+        'commAuth': ['Chantal Lumen'],
+        'comments': [
+            '🔥🔥🔥'
+        ],
+    },
+];
+
+let user = [{
+    'name': 'Klaus',
+    'image': 'src/img/profile/4.jpg',
+    'desc': 'Neu bei Instagram',
+}, {
+    'name': 'Hans',
+    'image': 'src/img/profile/2.jpg',
+    'desc': 'Beliebt',
+}, {
+    'name': 'Ralph',
+    'image': 'src/img/profile/3.jpg',
+    'desc': 'Rudi ist follower',
+}, {
+    'name': 'Chantal',
+    'image': 'src/img/profile/1.jpg',
+    'desc': 'Neu bei Instagram',
+}, ]
---
+++ templates.js
@@ -0,0 +1,98 @@
+function cardTemp(i) {
+    return `
+    <div class="card">
+    <div class="cardHeader">
+        <div class="left">
+            <img class="profileImg" style="width: 35px" src="${posts[i].profileImg}" />
+            <div class="flexCol">
+                <span style="font-family: 'circ'">${posts[i].author}</span>
+                <span style="font-size: 11px">${posts[i].description}</span>
+            </div>
+        </div>
+        <div class="right">⦁⦁⦁</div>
+    </div>
+    <div class="cardImg">
+        <img src="${posts[i].image}" width="100%" />
+    </div>
+    <div class="cardBtns">
+        <div class="left">
+            <img class="setInvert" onclick="likePost(${i})" id="likeBtn${i}" src="src/img/heart_empty.png" />
+            <img class="setInvert" id="commBtn${i}" src="src/img/comments.png" />
+            <img class="setInvert" src="src/img/message.png" />
+        </div>
+        <div class="right">
+            <img class="setInvert" onclick="markPost(${i})" id="markBtn${i}" src="src/img/mark-unfilled.png" />
+        </div>
+    </div>
+    <div class="cardLikes">
+        <span>Gefällt</span>
+        <div id="likes${i}">${posts[i].likes} </div>Mal
+    </div>
+    <div class="cardDesc">
+        <div>
+            ${posts[i].author}
+            <span class="descText">${posts[i].imgDesc}</span>
+        </div>
+    </div>
+    <div class="cardComm flexCol">
+        <span class="allComms"> Alle ${Math.floor(Math.random()*200)} Kommentare ansehen</span>
+        <div id="allComments${i}">
+            <div id="commentsDiv${i}">
+            </div>
+        </div>
+    </div>
+    <div class="cardPost">
+    <form onsubmit="postMsg(${i}); return false;">
+        <div>😊</div>
+
+        <input class="inputBox" id="postComment${i}" required placeholder="Kommentieren..." minlength="2" style="height: 14px"></input>
+
+        <button>Posten</button>
+        </div>
+    </form>
+</div>
+    `;
+}
+
+function allCommentsTemp(i, auth, comm) {
+    return `<div id="commentsDiv${i}" class="comments">
+            <span>${auth}</span>
+            <span class="descText">${comm}</span>
+        </div>`;
+}
+
+function myProfileTemp(name, img) {
+    return `
+        <div>
+            <img class="profileImg" style="width: 50px" src="${img}" />
+            <span>${name}</span>
+        </div>
+        <span class="profileLink" onclick="changeProfile()">Wechseln</span>
+    `;
+}
+
+function followerTemp(i) {
+    return `
+    <div class="sugProfile">
+        <div>
+            <img class="profileImg" style="width: 30px" src="${user[i].image}" />
+            <div class="mergeProfil">
+                <span>${user[i].name}</span>
+                <span style="color: #c0c0c0; font-family: sans-serif;">${user[i].desc}</span>
+            </div>
+        </div>
+        <span id="follow${i}" class="profileLink" onclick="follow(${i})">Folgen</span>
+    </div>
+    `;
+}
+
+function storysTemp(i) {
+    return `
+    <div class="storyProfile">
+        <div class="imgBgStory">
+            <img class="profileImg" style="width: 55px" src="${user[i].image}" />
+        </div>
+        <span>${user[i].name}</span>
+    </div>
+    `;
+}
